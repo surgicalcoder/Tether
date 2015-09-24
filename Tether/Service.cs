09e0de5523f8fa1bbc9946eace4667024bb4e71d
Monty A <monty@dotnetpro.co.uk>
2015-09-24 21:50:58 +0100

    Sending data to SD servers. Just need checks now.
    Oh, and need plugins to work!

--- Tether/Service.cs
+++ Tether/Service.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Timers;
 using NLog;
+using NLog.Fluent;
+using Tether.Plugins;
 using Topshelf;
 using Timer = System.Timers.Timer;
 
@@ -29,7 +32,37 @@ namespace Tether
 
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            throw new NotImplementedException();
+            var results = new Dictionary<string, object>();
+            var Checks = new List<ICheck>(); // TODO : REMOVE
+            
+                foreach (var check in Checks)
+            {
+                logger.Debug("{0}: start", check.GetType());
+                try
+                {
+                    var result = check.DoCheck();
+                    if (result != null)
+                    {
+                        // TODO: Something, something, plugin, metrics.
+                        logger.Debug("{0}: end", check.GetType());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex);
+                }
+            }
+            
+
+            try
+            {
+                var poster = new PayloadPoster(results);
+                poster.Post();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error with sending data to SD servers");
+            }
         }
 
         public bool Start(HostControl hostControl)
